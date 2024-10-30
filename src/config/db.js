7d9bdcf7a1aee3a844ef313c1327433e0744c00b
Hass Mohammed <hass@Hasss-MBP.communityfibre.co.uk>
2024-10-30 20:56:15 +0000

    upd - Fix connection to db

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/manualdb`, {logging: false});
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: 'mysql'
+});
 
 module.exports = sequelize;
