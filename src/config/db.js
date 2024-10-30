0cb234b189da3cef6fb57c1c0024b42cf88e2ca2
Hass Mohammed <hass@Hasss-MBP.communityfibre.co.uk>
2024-10-30 19:41:21 +0000

    upd - .env.example file

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('mysql://root:db-password@localhost:3306/manualdb', {logging: false});
+const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:3306/manualdb`, {logging: false});
 
 module.exports = sequelize;
